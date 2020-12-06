865f64ee4fc9224d450bc5de4c58062edad3168e
me <me@me.me>
2020-12-06 10:43:11 +0330

    slide 5

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-console.log(document.getElementById(1));
-let headerTitle = document.getElementById(1);
-let header = document.getElementById(2);
-console.log(headerTitle);
-headerTitle.textContent = 'header_one -> hello hatef javeri';
-headerTitle.innerText = 'header_one -> goodbye hatef javeri';
-console.log(headerTitle.innerText);
-headerTitle.innerHTML = '<h3>hello hatef javeri</h3>';
-header.style.borderBottom = 'solid 3px #000';
+let items = document.getElementsByClassName('nav');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'hatef javer slide 5';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(let i = 0; i < items.length; i++){
+  items[i].style.backgroundColor = '#f4367c';
+}
